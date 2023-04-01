c02b9e6bd810c88dcb165ff90a139b9e53837461
Fernando Nogueira <abert036@uottawa.ca>
2023-04-01 17:39:43 -0400

    wip

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,49 @@
+<div class="w-screen  min-h-screen bg-zinc-600 py-4">
+  <div class="container mx-auto bg-white shadow-2xl min-h-screen rounded-3xl">
+    <div class=" mx-auto prose prose-resume max-w-none p-4 ">
+      <!-- Heading   -->
+      <h1>Fernando Nogueira</h1>
+      <h2>
+        Ottawa, ON<br />
+        (609) 401-9906 &nbsp; abert036@uottawa.ca
+      </h2>
+
+      <hr class="border-dashed border-2 w-full" />
+
+      <main class="flex flex-col gap-4">
+        <!-- Professional Summary -->
+        <section class="card outline">
+          <div class="card-body">
+            <h3 class="m-0 card-title">Professional Summary</h3>
+            <p>
+              Undergraduate student at the University of Ottawa, pursuing an
+              Honours Bachelor of Science with Major in Computer Science and
+              Minor in Statistics (French Immersion Stream) . Self-taught and
+              driven by passion for technology. Focused on having a continuous
+              improvement mindset. I am a strong believer of the adoption of
+              faster, cleaner and maintainable technologies.
+            </p>
+          </div>
+        </section>
+
+        <section class="card outline">
+          <div class="card-body">
+            <h3 class="m-0  card-title">Experience</h3>
+            <div></div>
+            <h4>Full Stack Software Developer · Co-op</h4>
+            <h5>University of Ottawa, Ottawa, ON</h5>
+          </div>
+        </section>
+      </main>
+    </div>
+  </div>
+</div>
+
+<style>
+  /* A4 print styles */
+  @media print {
+    @page {
+      margin: 0;
+    }
+  }
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,4 +2,59 @@
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  theme: {
+    extend: {
+      typography: {
+        resume: {
+          // Primary color: 34bf49
+          // Neutral color: #333,
+          css: {
+            h1: {
+              textAlign: "center",
+              textTransform: "uppercase",
+              marginTop: "1rem",
+              marginBottom: "0rem",
+              color: "#34bf49",
+            },
+            hr: {
+              borderColor: "#333",
+              marginTop: "3rem",
+              marginBottom: "3rem",
+            },
+            h2: {
+              textAlign: "center",
+              marginTop: "1.5rem",
+              color: "#333",
+              fontSize: "1rem",
+            },
+            h3: {
+              textTransform: "uppercase",
+              color: "#34bf49",
+            },
+            h4: {
+              color: "#333",
+              marginLeft: "1rem",
+              marginBottom: "0rem",
+            },
+            h5: {
+              color: "#34bf49",
+              marginTop: "0rem",
+              marginLeft: "2rem",
+            },
+            p: {
+              color: "#333",
+              marginLeft: "1rem",
+            },
+            strong: {
+              color: "#000",
+            },
+            li: {
+              color: "#333",
+              marginLeft: "1rem",
+            },
+          },
+        },
+      },
+    },
+  },
 };
