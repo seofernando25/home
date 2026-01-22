aba7d163d2e8b4d3263f2e7f83054c812b46879e
Fernando <abert036@uottawa.ca>
2026-01-22 16:56:40 -0500

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	build: {
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					'home-components': [
-						'$lib/components/home/HomePage2.svelte',
-						'$lib/components/home/History.svelte'
-					]
-				}
-			}
-		},
 		chunkSizeWarningLimit: 1000
 	}
 });
